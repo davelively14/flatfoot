d6ef7a407b7f6b75e1046645229f0019ca73f2bb
Dave Lively <davelively14@gmail.com>
2017-04-01 09:17:52 -0400

    Added sessions to seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -7,3 +7,21 @@ alias Flatfoot.Clients
     password: Faker.Code.isbn
   }) end
 )
+
+Clients.create_user(%{
+  username: "dlively",
+  email: "dlively@resurgens.io",
+  password: "password"
+})
+
+users = Clients.list_users
+
+users |> Enum.each( fn(user) ->
+  Clients.login(%{
+    token: SecureRandom.urlsafe_base64(),
+    user_id: user.id
+  }) end
+)
+
+IO.inspect "Seed complete"
+IO.inspect "#{users |> length} users created"
