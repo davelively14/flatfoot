82343ffe02a8bf88d33a593cb754ca5f8f64bad8
Dave Lively <davelively14@gmail.com>
2017-04-10 17:50:43 -0400

    Updated target

--- lib/flatfoot/spade/target.ex
+++ lib/flatfoot/spade/target.ex
@@ -2,7 +2,11 @@ defmodule Flatfoot.Spade.Target do
   use Ecto.Schema
 
   schema "spade_targets" do
+    belongs_to :user, Flatfoot.Spade.User
+
     field :name, :string
+    field :relationship, :string
+    field :active, :boolean, default: true
 
     timestamps()
   end
