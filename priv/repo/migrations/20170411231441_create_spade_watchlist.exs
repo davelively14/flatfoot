4e2d784d7ccb0d850b9965048a6f43f16192bb37
Dave Lively <davelively14@gmail.com>
2017-04-11 20:01:45 -0400

    Change from :spade_watchlist to :spade_watchlists

--- priv/repo/migrations/20170411231441_create_spade_watchlist.exs
+++ priv/repo/migrations/20170411231441_create_spade_watchlist.exs
@@ -2,13 +2,13 @@ defmodule Flatfoot.Repo.Migrations.CreateSpadeWatchlist do
   use Ecto.Migration
 
   def change do
-    create table(:spade_watchlist) do
+    create table(:spade_watchlists) do
       add :user_id, references(:clients_users)
       add :name, :string
 
       timestamps()
     end
 
-    create index(:spade_watchlist, [:user_id])
+    create index(:spade_watchlists, [:user_id])
   end
 end
