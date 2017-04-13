0adb9b0c5ff3c47c87f81b5c853d09667ef1e0d9
Dave Lively <davelively14@gmail.com>
2017-04-12 22:56:31 -0400

    Added Suspect schema and migration

---
+++ lib/flatfoot/spade/suspect.ex
@@ -0,0 +1,15 @@
+defmodule Flatfoot.Spade.Suspect do
+  use Ecto.Schema
+
+  schema "spade_suspects" do
+    belongs_to :user, Flatfoot.Spade.User
+    # has_many :suspect_accounts
+
+    field :name, :string
+    field :category, :string
+    field :notes, :string
+    field :status, :boolean, default: true
+
+    timestamps()
+  end
+end
---
+++ priv/repo/migrations/20170413025344_create_spade_suspects.exs
@@ -0,0 +1,17 @@
+defmodule Flatfoot.Repo.Migrations.CreateSpadeSuspects do
+  use Ecto.Migration
+
+  def change do
+    create table(:spade_suspects) do
+      add :user_id, references(:clients_users)
+      add :name, :string
+      add :category, :string
+      add :notes, :text
+      add :status, :boolean
+
+      timestamps()
+    end
+
+    create index(:spade_suspects, [:user_id])
+  end
+end
