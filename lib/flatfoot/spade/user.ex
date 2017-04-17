97e1794b04b9ec85da2a1690c9171286227e6f1d
Dave Lively <davelively14@gmail.com>
2017-04-17 19:27:42 -0400

    Added has_many associations

--- lib/flatfoot/spade/user.ex
+++ lib/flatfoot/spade/user.ex
@@ -2,6 +2,8 @@ defmodule Flatfoot.Spade.User do
   use Ecto.Schema
 
   schema "clients_users" do
+    has_many :watchlists, Flatfoot.Spade.Watchlist, on_delete: :delete_all
+    has_many :wards, Flatfoot.Spade.Ward, on_delete: :delete_all
     field :email, :string
     field :username, :string
 
