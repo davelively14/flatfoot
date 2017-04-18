6189b99cfa60fc9982d266ffffe4384eec90d6e3
Dave Lively <davelively14@gmail.com>
2017-04-17 21:07:51 -0400

    Changed belongs_to from Archer.Backend to Spade.Backend

--- lib/flatfoot/spade/suspect_account.ex
+++ lib/flatfoot/spade/suspect_account.ex
@@ -3,7 +3,7 @@ defmodule Flatfoot.Spade.SuspectAccount do
 
   schema "spade_suspect_accounts" do
     belongs_to :suspect, Flatfoot.Spade.Suspect
-    belongs_to :backend, Flatfoot.Archer.Backend
+    belongs_to :backend, Flatfoot.Spade.Backend
 
     field :handle, :string
 
