4844960a3c0a79b8d9e739daecff2050cf9f648f
Dave Lively <davelively14@gmail.com>
2017-04-22 18:12:12 -0400

    Added module to schema

--- lib/flatfoot/spade/schema/backend.ex
+++ lib/flatfoot/spade/schema/backend.ex
@@ -3,9 +3,10 @@ defmodule Flatfoot.Spade.Backend do
 
   schema "archer_backends" do
     has_many :ward_accounts, Flatfoot.Spade.WardAccount, on_delete: :delete_all
-    
+
     field :name, :string
     field :url, :string
+    field :module, :string
 
     timestamps()
   end
