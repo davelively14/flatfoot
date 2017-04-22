927fdadf31e847737bbf95bb80838b964a4aca8d
Dave Lively <davelively14@gmail.com>
2017-04-22 09:39:01 -0400

    Moved create_ward_result/1 to SpadeInspector

--- lib/flatfoot/spade/spade.ex
+++ lib/flatfoot/spade/spade.ex
@@ -640,24 +640,6 @@ defmodule Flatfoot.Spade do
   alias Flatfoot.Spade.WardResult
 
   @doc """
-  Creates a ward result with valid params
-
-  ## Examples
-
-      iex> create_ward_result(%{from: "@bully", msg_id: "1234567890", msg_text: "you stink", rating: 25, ward_id: 4, backend_id: 3)
-      {:ok, %WardResult{from: "@bully", msg_id: "1234567890", msg_text: "you stink", rating: 25, ward_id: 4, backend_id: 3}}
-
-      iex> create_ward_result(%{})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_ward_result(attrs) do
-    %WardResult{}
-    |> ward_result_changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
   Given a ward id, returns a list of ward results for that ward.
 
   ## Examples
@@ -765,12 +747,5 @@ defmodule Flatfoot.Spade do
     suspect
     |> cast(attrs, [:handle])
     |> validate_required([:handle, :suspect_id, :backend_id])
-  end
-
-  defp ward_result_changeset(%WardResult{} = ward_result, attrs) do
-    ward_result
-    |> cast(attrs, [:ward_id, :backend_id, :rating, :from, :msg_id, :msg_text])
-    |> validate_required([:ward_id, :backend_id, :rating, :from, :msg_id, :msg_text])
-    |> validate_inclusion(:rating, 0..100)
   end
 end
