65318cd30567b9f20a571dd2e20ecaff67f7f041
Dave Lively <davelively14@gmail.com>
2017-05-08 22:13:04 -0400

    Fixed JS to fit convention

--- priv/static/js/application.js
+++ priv/static/js/application.js
@@ -82,19 +82,19 @@ __webpack_require__(1);
 "use strict";
 
 
-var elements = document.querySelectorAll('[data-submit^=parent]');
+var elements = document.querySelectorAll("[data-submit^=parent]");
 var len = elements.length;
 
 for (var i = 0; i < len; ++i) {
-  elements[i].addEventListener('click', function (event) {
+  elements[i].addEventListener("click", function (event) {
     var message = this.getAttribute("data-confirm");
     if (message === null || confirm(message)) {
       this.parentNode.submit();
-    };
+    }
     event.preventDefault();
     return false;
   }, false);
-};
+}
 
 /***/ }),
 /* 2 */
