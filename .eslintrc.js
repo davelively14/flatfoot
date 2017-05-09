429cc083d6a00aaaec0f81760e58e8a70fcb9868
Dave Lively <davelively14@gmail.com>
2017-05-08 22:06:57 -0400

    Added .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": [
+      "eslint:recommended"
+    ],
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console":0,
+        "no-unused-vars": ["error", {"argsIgnorePattern": "^_"}]
+    }
+};
