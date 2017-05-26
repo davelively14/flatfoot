33424add9d4573979910643980da0d62b876620b
Dave Lively <davelively14@gmail.com>
2017-05-25 23:00:43 -0400

    Added ward_id to WardAccountView

--- lib/flatfoot/web/views/spade/ward_account_view.ex
+++ lib/flatfoot/web/views/spade/ward_account_view.ex
@@ -4,6 +4,7 @@ defmodule Flatfoot.Web.WardAccountView do
   def render("ward_account.json", %{ward_account: ward_account}) do
     %{
       id: ward_account.id,
+      ward_id: ward_account.ward_id,
       handle: ward_account.handle,
       network: ward_account.backend.name,
       backend_module: ward_account.backend.module
