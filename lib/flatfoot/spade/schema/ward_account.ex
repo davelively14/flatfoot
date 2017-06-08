4a4b88fde8acbc55ee245582a127fc4422c13033
Dave Lively <davelively14@gmail.com>
2017-06-07 20:48:51 -0400

    Added on_delete: :delete_all

--- lib/flatfoot/spade/schema/ward_account.ex
+++ lib/flatfoot/spade/schema/ward_account.ex
@@ -4,7 +4,7 @@ defmodule Flatfoot.Spade.WardAccount do
   schema "spade_ward_accounts" do
     belongs_to :ward, Flatfoot.Spade.Ward
     belongs_to :backend, Flatfoot.Spade.Backend
-    has_many :ward_results, Flatfoot.Spade.WardResult
+    has_many :ward_results, Flatfoot.Spade.WardResult, on_delete: :delete_all
 
     field :handle, :string
 
