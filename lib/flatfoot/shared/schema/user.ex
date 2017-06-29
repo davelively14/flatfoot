e9fbc37dced21ea1f6a07e5e965308a1fa0b79bd
Dave Lively <davelively14@gmail.com>
2017-06-29 14:26:46 -0400

    Moved Shared.User inside a /schema directory, added Watchlists

--- lib/flatfoot/shared/schema/user.ex
+++ lib/flatfoot/shared/schema/user.ex
@@ -6,5 +6,6 @@ defmodule Flatfoot.Shared.User do
     has_many :notification_records, Flatfoot.Clients.NotificationRecord, on_delete: :delete_all
     has_many :blackout_options, Flatfoot.Clients.BlackoutOption, on_delete: :delete_all
     has_many :wards, Flatfoot.Spade.Ward, on_delete: :delete_all
+    has_many :watchlists, Flatfoot.Spade.Watchlist, on_delete: :delete_all
   end
 end
