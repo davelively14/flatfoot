81251bfdf176cc6813914d4fe131ad11100f2187
Dave Lively <davelively14@gmail.com>
2017-09-05 22:29:24 -0400

    Added a couple more tests

--- test/js/reducers/backends.spec.js
+++ test/js/reducers/backends.spec.js
@@ -9,6 +9,10 @@ describe('backends', () => {
     expect(backends(undefined, {})).to.eql(initialState);
   });
 
+  it('should return state by default', () => {
+    expect(backends(undefined, {type: 'JOHNSON_CITY_TN'})).to.eql(initialState);
+  });
+
   it('should return to initial state when LOGOUT called', () => {
     expect(backends(['Some state'], {})).to.eql(['Some state']);
     expect(backends(['Some state'], {type: LOGOUT})).to.eql(initialState);
@@ -17,5 +21,9 @@ describe('backends', () => {
   it('should set state to passed backends', () => {
     let set_backends = ['one', 'two'];
     expect(backends(undefined, {type: SET_BACKENDS, backends: set_backends})).to.eql(set_backends);
+  });
+
+  it('should throw a TypeError if no object passed', () => {
+    expect(backends.bind(undefined, undefined)).to.throw(TypeError);
   });
 });
