98cf1d7b2c923176410e5f344efa12294e66eeea
Dave Lively <davelively14@gmail.com>
2017-09-06 22:40:55 -0400

    Fixed verbiage to be consistent throughout tests

--- test/js/reducers/wards.spec.js
+++ test/js/reducers/wards.spec.js
@@ -16,37 +16,37 @@ describe('wards', () => {
     expect(wards(undefined, {type: 'NO_LEGIT_TYPE'})).to.eql(initialState);
   });
 
-  it('should add a ward to the current state when ADD_WARD called', () => {
+  it('should add a ward to the current state with ADD_WARD type', () => {
     expect(wards(undefined, {type: ADD_WARD, ward_params: wardParams1})).to.eql([wardParams1]);
     expect(wards([wardParams1], {type: ADD_WARD, ward_params: wardParams2})).to.eql([wardParams1, wardParams2]);
   });
 
-  it('ignores unrecognized params from passed ward_params when ADD_WARD called', () => {
+  it('ignores unrecognized params from passed ward_params with ADD_WARD type', () => {
     let verboseWardParams = Object.assign({}, wardParams1, {nothing: true, num: 5});
     expect(wards(undefined, {type: ADD_WARD, ward_params: verboseWardParams})).to.eql([wardParams1]);
   });
 
-  it('should remove a ward from the current state when REMOVE_WARD called', () => {
+  it('should remove a ward from the current state with REMOVE_WARD type', () => {
     expect(wards([wardParams1], {type: REMOVE_WARD, ward_id: wardParams1.id})).to.eql([]);
   });
 
-  it('should do nothing if bad id passed when REMOVE_WARD called', () => {
+  it('should do nothing if bad id passed with REMOVE_WARD type', () => {
     expect(wards([wardParams1], {type: REMOVE_WARD, ward_id: wardParams2.id})).to.eql([wardParams1]);
   });
 
-  it('should update params of a particular ward entry when UPDATE_WARD called', () => {
+  it('should update params of a particular ward entry with UPDATE_WARD type', () => {
     expect(wards([wardParams1, wardParams2], {type: UPDATE_WARD, ward_params: updatedWardParams1})).to.eql([updatedWardParams1, wardParams2]);
   });
 
-  it('should do nothing when UPDATE_WARD called and passed a ward without an already existing id', () => {
+  it('should do nothing with UPDATE_WARD type and passed a ward without an already existing id', () => {
     expect(wards([wardParams1, wardParams2], {type: UPDATE_WARD, ward_params: {id: 4}})).to.eql([wardParams1, wardParams2]);
   });
 
-  it('should set state to default when LOGOUT is called', () => {
+  it('should set state to default with LOGOUT type', () => {
     expect(wards([wardParams1], {type: LOGOUT})).to.eql(initialState);
   });
 
-  it('should set state to default when CLEAR_DASHBOARD is called', () => {
+  it('should set state to default with CLEAR_DASHBOARD type', () => {
     expect(wards([wardParams1], {type: CLEAR_DASHBOARD})).to.eql(initialState);
   });
 
