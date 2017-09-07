55b7b3bb260b17ec7795161524ad31595cf6f27b
Dave Lively <davelively14@gmail.com>
2017-09-07 18:30:01 -0400

    Completed session spec

--- test/js/reducers/session.spec.js
+++ test/js/reducers/session.spec.js
@@ -26,4 +26,33 @@ describe('session', () => {
     expect(startState.token).to.eq('asdf');
     expect(session(startState, {type: SET_TOKEN}).token).to.be.undefined;
   });
+
+  // TODO: passes phoenixToken instead of phoenix_token? Just be consistent
+  it('should assign phoenixToken the passed value with SET_PHOENIX_TOKEN type', () => {
+    expect(session(initialState, {type: SET_PHOENIX_TOKEN, phoenixToken: 'asdf'}).phoenixToken).to.eq('asdf');
+  });
+
+  it('should assign undefined to phoenixToken with SET_PHOENIX_TOKEN type and no phoenixToken', () => {
+    let startState = Object.assign({}, initialState, {phoenixToken: 'asdf'});
+    expect(startState.phoenixToken).to.eq('asdf');
+    expect(session(startState, {type: SET_PHOENIX_TOKEN}).phoenixToken).to.be.undefined;
+  });
+
+  it('should create and assign a socket object to socket with SET_SOCKET type and phoenixToken', () => {
+    expect(session(initialState, {type: SET_SOCKET, phoenixToken: 'asdf'}).socket.params.token).to.eq('asdf');
+  });
+
+  it('should create and assign a socket object with SET_SOCKET type and no phoenixToken, but with empty token params', () => {
+    expect(session(initialState, {type: SET_SOCKET}).socket.params.token).to.be.undefined;
+  });
+
+  it('should set socket to undefined with CLEAR_SOCKET type', () => {
+    let startState = Object.assign({}, initialState, {socket: 'asdf'});
+    expect(session(startState, {type: CLEAR_SOCKET}).socket).to.be.undefined;
+  });
+
+  it('should set state to initialState with LOGOUT type', () => {
+    let startState = Object.assign({}, initialState, {token: 'asdf', phoenixToken: '1234', socket: 'somesocket'});
+    expect(session(startState, {type: LOGOUT})).to.eql(initialState);
+  });
 });
